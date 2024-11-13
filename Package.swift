67dab6ac9582e94491581d61271123d5a1ff0d08
Jobin Kurian <jobin.kurian@netcorecloud.com>
2024-11-13 16:53:50 +0530

    v3.5.7

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         .target(name: "SmartechBaseSPM"),
         .binaryTarget(name: "Smartech",
-                      url: "https://github.com/NetcoreSolutions/SmartechBaseSPM/releases/download/3.5.6/Smartech.xcframework.zip",
-                     checksum: "310a13a2013049948ef8a81efb259366a5baa4810909622153cfaf31cc457faf")
+                      url: "https://github.com/NetcoreSolutions/SmartechBaseSPM/releases/download/3.5.7/Smartech.xcframework.zip",
+                     checksum: "c9963b4e5caf82d5195e41eb175bb2fd006f304d99eecfc8b6602564c776679c")
     ]
 )
